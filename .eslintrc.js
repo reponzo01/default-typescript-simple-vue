996a70cbcdf8cc1d8307e6993c4ad89d7dea2165
Emanuel Vargas <evargas@foundationsoft.com>
2021-10-05 11:38:51 -0400

    update to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,22 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
-  ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
-  rules: {
-    "prettier/prettier": ["warn",  {"endOfLine": "auto", "singleQuote": true, "tabWidth": 4}],
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    root: true,
+    env: {
+      node: true,
+    },
+    extends: [
+      "plugin:vue/essential",
+      "eslint:recommended",
+      "@vue/typescript/recommended",
+      "@vue/prettier",
+      "@vue/prettier/@typescript-eslint",
+    ],
+    parserOptions: {
+      ecmaVersion: 2020,
+    },
+    rules: {
+      "prettier/prettier": ["warn",  {"endOfLine": "auto", "singleQuote": true, "tabWidth": 4}],
+      "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+      "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+      "@typescript-eslint/no-inferrable-types": "off",
+    },
+  };
